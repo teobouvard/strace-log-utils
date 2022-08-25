c736b6ae00f8fe444504a3cc334d5fb5308553fe
Téo Bouvard <teobouvard@gmail.com>
2022-08-26 00:27:15 +0200

    Add syscall args splitting
    Only parsing raw comma-delimited strings for now.

--- src/parser.rs
+++ src/parser.rs
@@ -1,24 +1,37 @@
 use anyhow::{anyhow, Result};
-use nom::bytes::complete::take_until;
-use nom::bytes::streaming::tag;
+use nom::bytes::complete::{tag, take_till, take_until};
 use nom::character::complete::{digit1, multispace0};
 use nom::combinator::{map, map_res, opt, recognize};
+use nom::multi::separated_list0;
 use nom::sequence::{delimited, tuple};
 use nom::IResult;
 
 #[derive(Debug, PartialEq)]
+pub enum Arg<'a> {
+    Null,
+    String(&'a str),
+    Flags(Vec<&'a str>),
+}
+
+#[derive(Debug, PartialEq)]
 pub struct SysCall<'a> {
     name: &'a str,
-    args: &'a str,
+    args: Vec<Arg<'a>>,
     result: i8,
+}
+
+pub fn arg_raw_str(s: &str) -> IResult<&str, Arg> {
+    map(take_till(|c| c == ',' || c == ')'), |s: &str| {
+        Arg::String(s)
+    })(s)
 }
 
 pub fn syscall_name(s: &str) -> IResult<&str, &str> {
     take_until("(")(s)
 }
 
-pub fn syscall_args(s: &str) -> IResult<&str, &str> {
-    take_until(")")(s)
+pub fn syscall_args(s: &str) -> IResult<&str, Vec<Arg>> {
+    delimited(tag("("), separated_list0(tag(","), arg_raw_str), tag(")"))(s)
 }
 
 pub fn syscall_result(s: &str) -> IResult<&str, i8> {
@@ -31,7 +44,7 @@ pub fn parse_syscall_line(s: &str) -> IResult<&str, SysCall> {
     map(
         tuple((
             syscall_name,
-            delimited(tag("("), syscall_args, tag(")")),
+            syscall_args,
             delimited(multispace0, tag("="), multispace0),
             syscall_result,
         )),
@@ -48,7 +61,7 @@ pub fn parse_log_line(s: &str) -> Result<SysCall> {
 
 #[cfg(test)]
 mod tests {
-    use crate::parser::{parse_syscall_line, SysCall};
+    use crate::parser::{parse_syscall_line, Arg, SysCall};
 
     #[test]
     fn parse_openat() {
@@ -61,7 +74,11 @@ mod tests {
                 "",
                 SysCall {
                     name: "openat",
-                    args: r#"AT_FDCWD, "/etc/ld.so.cache", O_RDONLY|O_CLOEXEC"#,
+                    args: vec!(
+                        Arg::String("AT_FDCWD"),
+                        Arg::String(" \"/etc/ld.so.cache\""),
+                        Arg::String(" O_RDONLY|O_CLOEXEC"),
+                    ),
                     result: 3
                 }
             ))
